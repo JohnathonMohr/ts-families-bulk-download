baa258b3b7f93f351b3918dfddb62d433ecf6783
Johnathon Mohr <JohnathonMohr@users.noreply.github.com>
2024-07-13 22:37:37 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# ts-families-bulk-download
-Tool to download pictures from Teaching Strategies Families in bulk.
+# Bulk download pictures from TS Families
+This repository hosts a PowerShell script that can be used to download pictures of your dependent(s) in bulk from the Teaching Strategies Families platform (a.k.a. TS Families or "Tadpoles").
+
+## How the script works
+When the script is run, it will prompt for your TS Families username and password. After entering these, the script will log in, find your dependents, download their profile picture(s) to the directory the script is running in, and then search for all event records in the platform (starting with the current date, and going as far back as your dependent(s) were enrolled). Once this is complete, it will download all pictures found and save them under a folder with that dependent's name. Each picture will be named by the date it was taken followed by a number, so if there are multiple pictures for a day, they will be numbered (1), (2), etc...
+
+## Downloading the script
+Click on TSFamiliesDownload.ps1 in file list above. Then click the Download button (top right side of file) when the script loads in the browser.
+
+![Download button](images/download.png)
+
+## Make sure you can run PowerShell
+### Windows
+PowerShell is built into Windows, but you may have to run a couple commands to allow the script to be executed by Windows, since by default Windows does not allow downloaded scripts to be run for security reasons.
+
+To open PowerShell:
+1. Click the Start button or hit the Windows key on your keyboard.
+2. Search for PowerShell and click it (or hit Enter).
+
+![Download button](images/powershell.png)
+
+3. Make sure you can execute the script - copy and paste this command into the PowerShell window to make sure it can run any script.
+
+**NOTE**: This will only allow the *current PowerShell session* to run any script. Once the window is closed, it will revert back to default, which is more restrictive.
+
+```
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted
+```
+
+### Mac OS
+PowerShell can be installed on Mac OS by following [Microsoft's instructions](https://learn.microsoft.com/powershell/scripting/install/installing-powershell-on-macos?view=powershell-7.4).
+
+## Running the script
+
+1. In the PowerShell window, change the working directory to the location the script was downloaded to.
+
+- For example, if downloaded to the standard *Downloads* location in Chrome/Edge/etc., you can copy and run this command:
+    ```
+    cd $env:userprofile\downloads
+    ```
+
+2. Run the script.
+```
+.\TSFamilesDownload.ps1
+```
+
+## Advanced options
+Here are some optional arguments that can be specified.
+
+- `-earliestDate <date>` (like "2024-05-01"): Instead of downloading images as far back as your dependent was enrolled, only download images as early as this date.
+- `-latestDate <date>` (like "2024-05-01"): Instead of downloading images starting with the current date, only download images as old as the date specified.
+
+## Known shortcomings
+- Dependents commonly have images of their leftover lunch. While I understand what it would take technically to filter these out, I don't have the time to implement this, so all lunch images will be included in the bulk download.
+- I'm not sure how reliably images are sorted when discovered from the service. As a result, running the script multiple times may label multiple images in the same day differently. Therefore, if the script is run again, it will overwrite all images already downloaded and download them again, and the names of the image (e.g. 2024-05-01 (1), 2024-05-01 (2), ...) may get swapped or mixed around.
+  - As a result, it's safest not to interrupt the script while downloading a second time to ensure no images get overwritten with a duplicate of a different image.
+  - If you just want to run it again to download new pictures that weren't available last time, I suggest using the [date range options above](#advanced-options).
+
+## Have a problem or question?
+Let me know by [submitting an issue](https://github.com/JohnathonMohr/ts-families-bulk-download/issues/new)!.
+
+However, please understand I don't have much time to dedicate to this, so I may or may not be able to help.
---
+++ TSFamilesDownload.ps1
@@ -0,0 +1,185 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $earliestDate,
+
+    [Parameter()]
+    [string]
+    $latestDate
+)
+
+if ($earliestDate -ne "") {
+    try {
+        $earliestDate = [DateTime]::Parse($earliestDate).Date
+    } catch {
+        Write-Host "Invalid date format. Please use a format like '2021-01-01'"
+        return
+    }
+}
+if ($latestDate -ne "") {
+    try {
+        $latestDate = [DateTime]::Parse($latestDate).Date.AddDays(1).AddSeconds(-1)
+    } catch {
+        Write-Host "Invalid date format. Please use a format like '2021-01-01'"
+        return
+    }
+} else {
+    $latestDate = [DateTime]::Now
+}
+
+$userName = Read-Host -Prompt "Tadpoles username"
+$password = Read-Host -Prompt "Tadpoles password" -AsSecureString
+
+$eventRangeInDays = 30
+
+# Get the local time zone UTC offset, formatted as a string, e.g., +02:00 or -07:00, and URI-escaped
+$localTimeZone = [System.TimeZoneInfo]::Local
+$utcOffset = $localTimeZone.BaseUtcOffset
+$utcOffsetString = [uri]::EscapeDataString([string]::Format("{0}{1:D2}:{2:D2}", $(if($utcOffset.Hours -ge 0) {"+"} else {"-"}), [Math]::Abs($utcOffset.Hours), $utcOffset.Minutes))
+$usesDst = if ($localTimeZone.SupportsDaylightSavingTime) {1} else {0}
+
+$tadpolesApi = "https://www.tadpoles.com/remote/v1"
+$authBody = "email=" + [uri]::EscapeDataString($userName) + "&service=tadpoles&password=" + [uri]::EscapeDataString([Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)))
+
+# Known issue: $admitBody must specify `&tz=<time zone>`, but I don't have a good way to get the string format necessary from the local time zone.
+# So &tz=America%2FLos_Angeles is hard-coded for now.
+$admitBody = "state=client&tz=America%2FLos_Angeles&battery_level=-1&locale=en-US&platform_version=17.5.1&logged_in=1&uses_dst=$usesDst&utc_offset=$utcOffsetString&v=2"
+
+# Log in to tadpoles
+try {
+    $resp = Invoke-WebRequest -Method Post -Uri https://www.tadpoles.com/auth/login -Body $authBody -SessionVariable webSession 
+    $resp = Invoke-WebRequest -Method Post -Uri $($tadpolesApi + "/athome/admit") -Body $admitBody -WebSession $webSession
+}
+catch {
+    Write-Host "Failed to log in. Please check your username and password."
+    return
+}
+
+Write-Host "Logged in to tadpoles.com as $userName"
+
+# Get dependants
+$resp = Invoke-WebRequest -Method Get -Uri $($tadpolesApi + "/parameters?include_all_kids=true") -WebSession $webSession
+
+$dependants = @{}
+($resp.Content | ConvertFrom-Json).memberships[0].dependants.GetEnumerator() | ForEach-Object {
+    $dependants[$_.key] = @{
+        name = $_.display_name
+        firstName = $_.first_name
+        enrolled = Get-Date $_.ed
+        hasImage = $_.has_image
+        image = $_.image_attachment
+    }
+}
+
+$earliestEnrolledDate = Get-Date
+
+Write-Host "Dependants:"
+$dependants.GetEnumerator() | ForEach-Object {
+    Write-Host "    $($_.Value.name) (enrolled $($_.Value.enrolled.ToShortDateString()))"
+
+    if ($_.Value.hasImage) {
+        Invoke-WebRequest -Method Get -Uri $($tadpolesApi + "/attachment?thumbnail=false&key=$($_.Value.image)") -WebSession $webSession -OutFile "$($_.Value.name) Profile Pic.jpg"
+    }
+
+    if (-not (Test-Path $_.Value.name)) {
+        New-Item -Path $_.Value.name -ItemType Directory > $null
+    }
+
+    if ($_.Value.enrolled -lt $earliestEnrolledDate) {
+        $earliestEnrolledDate = $_.Value.enrolled
+    }
+}
+
+$date = [DateTime]$latestDate
+
+if ($earliestDate -eq "") {
+    $earliestDate = [DateTime]$earliestEnrolledDate
+}
+
+Write-Host "Finding events"
+
+$imageKeys = @()
+
+$imagesPerDate = @{}
+$imageCountPerDate = @{}
+
+$continuationToken = $null
+while ($date -ge $earliestDate) {
+    $eventCount = 0
+
+    $earliestRequestDate = ([DateTimeOffset]$date.AddDays(-$eventRangeInDays))
+    $latestRequestDate = ([DateTimeOffset]$date)
+
+    Write-Host "   $($earliestRequestDate.ToString("d")) - $($latestRequestDate.ToString("d"))"
+
+    do {
+        $queryParams = "num_events=100&state=client&direction=range&earliest_event_time=$($earliestRequestDate.ToUnixTimeSeconds())&latest_event_time=$($latestRequestDate.ToUnixTimeSeconds())"
+
+        if ($null -ne $continuationToken) {
+            $queryParams += "&cursor=$continuationToken"
+        }
+
+        $resp = Invoke-WebRequest -Method Get -Uri $($tadpolesApi + "/events?$queryParams") -WebSession $webSession
+
+        $events = $resp.Content | ConvertFrom-Json
+
+        $continuationToken = $events.cursor
+
+        $events.events | Where-Object { "Activity" -eq $_.type } | ForEach-Object {
+            $attachments = $_.new_attachments
+            $member = $_.member
+            
+            if (-not $imagesPerDate.ContainsKey($_.event_date)) {
+                $imagesPerDate[$_.event_date] = @()
+                $imageCountPerDate[$_.event_date] = 0
+            }
+
+            $eventDate = $_.event_date
+            $attachments | ForEach-Object {
+                $imagesPerDate[$eventDate] += @{
+                    key = $_.key
+                    member = $member
+                }
+
+                $imageCountPerDate[$eventDate] = $imageCountPerDate[$eventDate] + 1
+
+                $imageKeys += @{
+                    key = $_.key
+                    childName = $dependants[$member].name
+                    uri = $tadpolesApi + "/attachment?thumbnail=false&key=$($_.key)"
+                    imageName = "$eventDate ($($imageCountPerDate[$eventDate])).jpg"
+                }
+            }
+
+            $eventCount++
+        }
+    } while ($null -ne $continuationToken)
+
+    $date = $date.AddDays(-$eventRangeInDays)
+}
+
+# Set up synced hashtable to track parallel image download progress
+$completedImages = @{}
+$imageKeys | ForEach-Object {
+    $completedImages[$_.key] = $false
+}
+$sync = [System.Collections.Hashtable]::Synchronized($completedImages)
+
+$totalImages = $imageKeys.Count
+Write-Host "Downloading $totalImages images"
+
+$downloadJob = $imageKeys | ForEach-Object -ThrottleLimit 5 -AsJob -Parallel {
+    $syncCopy = $using:sync
+    $resp = Invoke-WebRequest -Method Get -Uri $_.uri -WebSession $using:webSession -OutFile "$($_.childName)\$($_.imageName)"
+
+    $syncCopy[$_.key] = $true
+}
+
+while ($downloadJob.State -ne "Completed") {
+    $progress = $sync.Values | Where-Object { $_ -eq $true } | Measure-Object | Select-Object -ExpandProperty Count
+    Write-Progress -Activity "Downloading Images" -Status "Progress: $progress/$totalImages" -PercentComplete (($progress / $totalImages) * 100)
+    Start-Sleep -Seconds 1
+}
+
+Write-Host "Download complete"
